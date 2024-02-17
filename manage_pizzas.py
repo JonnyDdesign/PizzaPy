983079cb4989750cfa71a86372f4caaa4fafd91f
JonnyDdesign <jondesserres@gmail.com>
2024-02-16 23:43:27 -0700

    Changed the file names to represent the two operations performed. Added initial list for specialty pizzas under the manage pizzas file.

---
+++ manage_pizzas.py
@@ -0,0 +1,24 @@
+#Manage specialty pizzas section
+
+specialty_pizzas = [
+    {
+        "name": "Margherita",
+        "toppings": ["Tomatoes", "Mozzarella", "Basil"]
+    },
+    {
+        "name": "Hawaiian",
+        "toppings": ["Ham", "Pineapple"]
+    },
+    {
+        "name": "White",
+        "toppings": ["Mozzarella", "Ricotta", "Garlic"]
+    },
+    {
+        "name": "Veggie Supreme",
+        "toppings": ["Green peppers", "Black olives", "Tomatoes", "Mushrooms", "Onions"]
+    },
+    {
+        "name": "Meat Galore",
+        "toppings": ["Pepperoni", "Sausage", "Bacon", "Ham", "Meatballs"]
+    }
+]
--- manage_toppings.py
+++ manage_toppings.py
@@ -54,4 +54,4 @@ def main():
     pizza_list.display_toppings()
 
 if __name__ == "__main__":
-    main()    
+    main()
